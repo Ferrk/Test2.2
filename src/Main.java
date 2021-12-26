9ba5f084df0f213d75a52fa93c2c8c16fde907db
Ferrk <qeri4gyw@mail.ru>
2021-12-26 16:11:09 +0300

    create app

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,22 @@
 public class Main {
     public static void main(String[] args) {
+        int currentAccount;
+        currentAccount = 100; // Текущий счет клиента
+        int depositAmount = 100; // Сумма пополнения счета
+        int oneBonus = 100;  // бонус за каждые 100р
+        int bonus;
+        int sum;
+        if (depositAmount >= 1000) {
+            bonus = depositAmount / oneBonus;
+            sum = currentAccount + depositAmount + bonus;
+        } else {
+            sum = currentAccount + depositAmount;
+            bonus = 0;
+        }
 
-
-        System.out.println();
+        System.out.println("На Вашем счете: " + sum + " рублей");
+        System.out.println("Ваш бонус " + bonus + " рублей");
     }
 }
+
 
